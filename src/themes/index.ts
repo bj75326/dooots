19d3fce459fe6185b940265db13962054a35fba7
Bill_Ji <superice_gy@163.com>
2020-08-26 18:33:27 +0800

    css

--- src/themes/index.ts
+++
@@ -1,7 +0,0 @@
-import fs from 'fs';
-import path from 'path';
-import lessToJs from 'less-vars-to-js';
-
-export default lessToJs(
-  fs.readFileSync(path.join(__dirname, `./skins/default.less`), 'utf8'),
-);
