d2cd6c28cd405533b39c3a333c015333b64b86d0
Bill_Ji <superice_gy@163.com>
2020-10-27 15:56:52 +0800

    🍌 tag component

--- src/pages/anki/components/EditableTagGroup/index.tsx
+++ src/pages/anki/components/EditableTagGroup/index.tsx
@@ -1,13 +1,102 @@
-import React, { useState } from 'react';
-import { Tag } from 'antd';
+import React, { useState, useRef, useEffect } from 'react';
+import { Tag, Input, Tooltip } from 'antd';
+import { PlusOutlined } from '@ant-design/icons';
 
 export interface EditableTagGroupProps {
   className?: string;
+  tags: string[];
 }
 
 const EditableTagGroup: React.FC<EditableTagGroupProps> = props => {
-  const [inputVisible, setInputVsible] = useState(false);
-  const [] = useState();
+  const [inputVisible, setInputVisible] = useState(false);
+  const [inputValue, setInputValue] = useState('');
+  const [editInputIndex, setEditInputIndex] = useState(-1);
+  const [editInputValue, setEditInputValue] = useState('');
+
+  const inputRef = useRef<Input>();
+
+  const { tags } = props;
+
+  const handleEditInputChange = () => {};
+
+  const handleEditInputConfirm = () => {};
+
+  const handleClose = (tag: string) => {};
+
+  const handleInputChange = () => {};
+
+  const handleInputConfirm = () => {};
+
+  const showInput = () => {
+    setInputVisible(true);
+  };
+
+  useEffect(() => {
+    if (inputRef.current) {
+      inputRef.current.focus();
+    }
+  }, [editInputIndex, editInputValue]);
+
+  useEffect(() => {}, [inputVisible]);
+
+  return (
+    <>
+      {tags.map((tag, index) => {
+        if (editInputIndex === index) {
+          return (
+            <Input
+              ref={inputRef as React.RefObject<Input>}
+              key={tag}
+              size="small"
+              value={editInputValue}
+              onChange={handleEditInputChange}
+              onBlur={handleEditInputConfirm}
+              onPressEnter={handleEditInputConfirm}
+            />
+          );
+        }
+
+        const isLongTag = tag.length > 20;
+
+        const tagElem = (
+          <Tag key={tag} closable onClose={() => handleClose(tag)}>
+            <span
+              onDoubleClick={e => {
+                setEditInputIndex(index);
+                setEditInputValue(tag);
+                e.preventDefault();
+              }}
+            >
+              {isLongTag ? `${tag.slice(0, 20)}` : tag}
+            </span>
+          </Tag>
+        );
+
+        return isLongTag ? (
+          <Tooltip title={tag} key={tag}>
+            {tagElem}
+          </Tooltip>
+        ) : (
+          tagElem
+        );
+      })}
+      {inputVisible && (
+        <Input
+          type="text"
+          size="small"
+          value={inputValue}
+          onChange={handleInputChange}
+          onBlur={handleInputConfirm}
+          onPressEnter={handleInputConfirm}
+        />
+      )}
+      {!inputVisible && (
+        <Tag onClick={showInput}>
+          <PlusOutlined /> New Tag
+        </Tag>
+      )}
+    </>
+  );
 };
 
 export default EditableTagGroup;
