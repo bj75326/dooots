ceb06665a1ab836eb95a3d87a19747266b7f374f
bj75326 <superice_gy@163.com>
2020-11-23 17:45:46 +0800

    🚈 deck page

--- src/pages/anki/deck/index.tsx
+++ src/pages/anki/deck/index.tsx
@@ -1,9 +1,20 @@
 import React from 'react';
+import MainSearch from '../components/MainSearch';
+import { useIntl } from 'umi';
+import NewCard from './components/NewCard';
 
-class AnkiDeck extends React.Component {
-  render() {
-    return <>AnkiDeck</>;
-  }
-}
+import styles from './style.less';
+
+interface AnkiDeckProps {}
+
+const AnkiDeck: React.FC<AnkiDeckProps> = props => {
+  const { formatMessage } = useIntl();
+
+  return (
+    <div className={styles.wrapper}>
+      <MainSearch placeholder={} />
+    </div>
+  );
+};
 
 export default AnkiDeck;
