5922973830d14196ceedf7fa3d0d1a394469e654
bj75326 <superice_gy@163.com>
2020-11-25 01:19:05 +0800

    🚂 zzz

--- src/pages/anki/deck/index.tsx
+++ src/pages/anki/deck/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import MainSearch from '../components/MainSearch';
 import { useIntl } from 'umi';
 import NewCard from './components/NewCard';
-import { Form, Select, Row, Col } from 'antd';
+import { Form, Select, Row, Col, Space, Button } from 'antd';
 
 import styles from './style.less';
 
@@ -21,11 +21,12 @@ const AnkiDeck: React.FC<AnkiDeckProps> = props => {
 
   const filterForm = (
     <Form form={form} name="deckFilter" className={styles.form}>
-      <Row>
+      <Row gutter={16}>
         <Col span={8}>
           <Form.Item
             name="status"
             label={formatMessage({ id: 'anki.deck.filter.status.label' })}
+            labelCol={{ flex: '0 0 120px' }}
           >
             <Select>
               <Option value="all">
@@ -44,37 +45,48 @@ const AnkiDeck: React.FC<AnkiDeckProps> = props => {
           <Form.Item
             name="rate"
             label={formatMessage({ id: 'anki.deck.filter.rate.label' })}
+            labelCol={{ flex: '0 0 120px' }}
           >
             <Select>
               <Option value="4">
-                {formatMessage({ id: 'anki.deck.filter.rate.underfour' })}
+                {formatMessage({ id: 'anki.deck.filter.rate.underFour' })}
               </Option>
               <Option value="3">
-                {formatMessage({ id: 'anki.deck.filter.rate.underthree' })}
+                {formatMessage({ id: 'anki.deck.filter.rate.underThree' })}
               </Option>
               <Option value="2">
-                {formatMessage({ id: 'anki.deck.filter.rate.undertwo' })}
+                {formatMessage({ id: 'anki.deck.filter.rate.underTwo' })}
               </Option>
             </Select>
           </Form.Item>
         </Col>
-        <Col span={8}></Col>
-        <Col span={8}></Col>
+        <Col span={8}>
+          <Form.Item
+            name="tags"
+            label={formatMessage({ id: 'anki.deck.filter.tags' })}
+            labelCol={{ flex: '0 0 120px' }}
+          ></Form.Item>
+        </Col>
+        <Col span={8}>
+          <Form.Item>
+            <Space>
+              <Button>{formatMessage({ id: 'anki.deck.filter.reset' })}</Button>
+            </Space>
+          </Form.Item>
+        </Col>
       </Row>
     </Form>
   );
 
   return (
     <div className={styles.wrapper}>
-      <MainSearch
+      {/* <MainSearch
         placeholder={formatMessage({ id: 'anki.search.card.placeholder' })}
         onSearch={handleSearch}
-      />
+      /> */}
+      <div className={styles.filter}>{filterForm}</div>
       <div className={styles.content}>
-        <div className={styles.filter}>{filterForm}</div>
-        <div className={styles.cards}>
-          <NewCard />
-        </div>
+        <NewCard />
       </div>
     </div>
   );
--- src/pages/anki/deck/locales/zh-CN.ts
+++ src/pages/anki/deck/locales/zh-CN.ts
@@ -3,4 +3,12 @@ export default {
   'anki.deck.new': '新建',
   'anki.deck.upload': '上传',
   'anki.deck.filter.status.label': '卡片状态',
+  'anki.deck.filter.status.all': '全部',
+  'anki.deck.filter.status.wip': '今日卡片',
+  'anki.deck.filter.status.overdue': '已逾期',
+  'anki.deck.filter.rate.label': '最近得分',
+  'anki.deck.filter.rate.all': '不限',
+  'anki.deck.filter.rate.underFour': '低于4分',
+  'anki.deck.filter.rate.underThree': '低于3分',
+  'anki.deck.filter.rate.underTwo': '低于2分',
 };
