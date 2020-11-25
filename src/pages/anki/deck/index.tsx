7c5055ad9c534d749c18899b6b3546ee1f7e63e2
bj75326 <superice_gy@163.com>
2020-11-26 01:24:00 +0800

    🚛 collapse animation

--- src/pages/anki/deck/index.tsx
+++ src/pages/anki/deck/index.tsx
@@ -1,9 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
 import MainSearch from '../components/MainSearch';
 import { useIntl } from 'umi';
 import NewCard from './components/NewCard';
 import { Form, Select, Row, Col, Space, Button } from 'antd';
 import { FilterOutlined } from '@ant-design/icons';
+import Animate from 'rc-animate';
 
 import styles from './style.less';
 
@@ -11,89 +12,113 @@ const { Option } = Select;
 
 interface AnkiDeckProps {}
 
+interface FilterProps {
+  visible: boolean;
+}
+
+const Filter: React.FC<FilterProps> = props => {
+  const form = Form.useForm()[0];
+  const { visible } = props;
+  const { formatMessage } = useIntl();
+
+  return (
+    <div
+      className={styles.filter}
+      style={{ display: visible ? 'block' : 'none' }}
+    >
+      <Form form={form} name="deckFilter" className={styles.form}>
+        <Row gutter={16}>
+          <Col span={6}>
+            <Form.Item
+              name="status"
+              label={formatMessage({ id: 'anki.deck.filter.status.label' })}
+              labelCol={{ flex: '0 0 90px' }}
+            >
+              <Select>
+                <Option value="all">
+                  {formatMessage({ id: 'anki.deck.filter.status.all' })}
+                </Option>
+                <Option value="wip">
+                  {formatMessage({ id: 'anki.deck.filter.status.wip' })}
+                </Option>
+                <Option value="overdue">
+                  {formatMessage({ id: 'anki.deck.filter.status.overdue' })}
+                </Option>
+                <Option value="done">
+                  {formatMessage({ id: 'anki.deck.filter.status.done' })}
+                </Option>
+              </Select>
+            </Form.Item>
+          </Col>
+          <Col span={6}>
+            <Form.Item
+              name="rate"
+              label={formatMessage({ id: 'anki.deck.filter.rate.label' })}
+              labelCol={{ flex: '0 0 90px' }}
+            >
+              <Select>
+                <Option value="all">
+                  {formatMessage({ id: 'anki.deck.filter.rate.all' })}
+                </Option>
+                <Option value="4">
+                  {formatMessage({ id: 'anki.deck.filter.rate.underFour' })}
+                </Option>
+                <Option value="3">
+                  {formatMessage({ id: 'anki.deck.filter.rate.underThree' })}
+                </Option>
+                <Option value="2">
+                  {formatMessage({ id: 'anki.deck.filter.rate.underTwo' })}
+                </Option>
+              </Select>
+            </Form.Item>
+          </Col>
+          <Col span={8}>
+            <Form.Item
+              name="tags"
+              label={formatMessage({ id: 'anki.deck.filter.tags' })}
+              labelCol={{ flex: '0 0 90px' }}
+            >
+              <Select mode="tags"></Select>
+            </Form.Item>
+          </Col>
+          <Col span={4} style={{ textAlign: 'right' }}>
+            <Form.Item>
+              <Space>
+                <Button shape="round">
+                  {formatMessage({ id: 'anki.deck.filter.reset' })}
+                </Button>
+                <Button type="primary" shape="round">
+                  {formatMessage({ id: 'anki.deck.filter.filter' })}
+                </Button>
+              </Space>
+            </Form.Item>
+          </Col>
+        </Row>
+      </Form>
+    </div>
+  );
+};
+
 const AnkiDeck: React.FC<AnkiDeckProps> = props => {
   const { formatMessage } = useIntl();
+
+  const [filterCollapsed, setFilterCollapsed]: [boolean, any] = useState(false);
 
   const handleSearch = (value: string) => {
     console.log(value);
   };
 
-  const form = Form.useForm()[0];
-
-  const filterForm = (
-    <Form form={form} name="deckFilter" className={styles.form}>
-      <Row gutter={16}>
-        <Col span={6}>
-          <Form.Item
-            name="status"
-            label={formatMessage({ id: 'anki.deck.filter.status.label' })}
-            labelCol={{ flex: '0 0 90px' }}
-          >
-            <Select>
-              <Option value="all">
-                {formatMessage({ id: 'anki.deck.filter.status.all' })}
-              </Option>
-              <Option value="wip">
-                {formatMessage({ id: 'anki.deck.filter.status.wip' })}
-              </Option>
-              <Option value="overdue">
-                {formatMessage({ id: 'anki.deck.filter.status.overdue' })}
-              </Option>
-              <Option value="done">
-                {formatMessage({ id: 'anki.deck.filter.status.done' })}
-              </Option>
-            </Select>
-          </Form.Item>
-        </Col>
-        <Col span={6}>
-          <Form.Item
-            name="rate"
-            label={formatMessage({ id: 'anki.deck.filter.rate.label' })}
-            labelCol={{ flex: '0 0 90px' }}
-          >
-            <Select>
-              <Option value="all">
-                {formatMessage({ id: 'anki.deck.filter.rate.all' })}
-              </Option>
-              <Option value="4">
-                {formatMessage({ id: 'anki.deck.filter.rate.underFour' })}
-              </Option>
-              <Option value="3">
-                {formatMessage({ id: 'anki.deck.filter.rate.underThree' })}
-              </Option>
-              <Option value="2">
-                {formatMessage({ id: 'anki.deck.filter.rate.underTwo' })}
-              </Option>
-            </Select>
-          </Form.Item>
-        </Col>
-        <Col span={8}>
-          <Form.Item
-            name="tags"
-            label={formatMessage({ id: 'anki.deck.filter.tags' })}
-            labelCol={{ flex: '0 0 90px' }}
-          >
-            <Select mode="tags"></Select>
-          </Form.Item>
-        </Col>
-        <Col span={4} style={{ textAlign: 'right' }}>
-          <Form.Item>
-            <Space>
-              <Button shape="round">
-                {formatMessage({ id: 'anki.deck.filter.reset' })}
-              </Button>
-              <Button type="primary" shape="round">
-                {formatMessage({ id: 'anki.deck.filter.filter' })}
-              </Button>
-            </Space>
-          </Form.Item>
-        </Col>
-      </Row>
-    </Form>
-  );
+  const toggleFilter = () => {
+    setFilterCollapsed((filterCollapsed: boolean) => !filterCollapsed);
+  };
 
   const extra = (
-    <Button type="primary" shape="circle" className={styles.searchExtra}>
+    <Button
+      type="primary"
+      shape="circle"
+      className={styles.searchExtra}
+      onClick={toggleFilter}
+    >
       <FilterOutlined />
     </Button>
   );
@@ -106,7 +131,13 @@ const AnkiDeck: React.FC<AnkiDeckProps> = props => {
         extra={extra}
         className={styles.search}
       />
-      <div className={styles.filter}>{filterForm}</div>
+      <Animate
+        showProp={'visible'}
+        transitionName="ant-motion-collapse-legacy"
+        component=""
+      >
+        <Filter visible={filterCollapsed} />
+      </Animate>
       <div className={styles.content}>
         <NewCard />
       </div>
